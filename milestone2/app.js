8498ab27a99b87109306c5eb66a9d1bf8bf4084d
Miyka96 <micaelaseminara@outlook.it>
2022-03-31 18:58:14 +0200

    progress

--- milestone2/app.js
+++ milestone2/app.js
@@ -2,7 +2,22 @@ const app = new Vue(
     {
         el: "#root",
         data: {
-            
-        }
+            dischi: [],
+        },
+        methods: {
+            fetchData() {
+                axios.get('../milestone2/server.php')
+                .then( res => {
+                    const results = res.data;
+                    console.log(res);
+                })
+                .catch(err => {
+                    console.warn(err.response);
+                });
+            },
+        },
+        created() {
+            this.fetchData();
+        },
     }
 )
